ec627dacb9de8c5a1192ccd21a543ce30088db6b
hiukim <hiukim528@gmail.com>
2023-03-05 13:32:50 -0800

    add threeJS version

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import React, { useState } from 'react';
-import 'aframe';
-import 'mind-ar/dist/mindar-image-aframe.prod.js';
 import './App.css';
 import MindARViewer from './mindar-viewer';
+import MindARThreeViewer from './mindar-three-viewer';
 
 function App() {
-  const [started, setStarted] = useState(false);
+  const [started, setStarted] = useState(null);
+
   return (
     <div className="App">
       <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
 
-      <div>
-        {!started && <button onClick={() => {setStarted(true)}}>Start</button>}
-        {started && <button onClick={() => {setStarted(false)}}>Stop</button>}
+      <div className="control-buttons">
+        {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
+        {started === null && <button onClick={() => {setStarted('three')}}>Start ThreeJS version</button>}
+        {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}
       </div>
 
-      {started && (
+      {started === 'aframe' && (
         <div className="container">
           <MindARViewer/>
           <video></video>
+        </div>
+      )}
+
+      {started === 'three' && (
+        <div className="container">
+          <MindARThreeViewer />
         </div>
       )}
     </div>
---
+++ src/mindar-three-viewer.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react';
+import {MindARThree} from 'mind-ar/dist/mindar-image-three.prod.js';
+import * as THREE from 'three';
+
+export default () => {
+  const containerRef = useRef(null);
+
+  useEffect(() => {
+    const mindarThree = new MindARThree({
+      container: containerRef.current,
+      imageTargetSrc: "https://cdn.jsdelivr.net/gh/hiukim/mind-ar-js@1.2.0/examples/image-tracking/assets/card-example/card.mind"
+    });
+    const {renderer, scene, camera} = mindarThree;
+    const anchor = mindarThree.addAnchor(0);
+    const geometry = new THREE.PlaneGeometry(1, 0.55);
+    const material = new THREE.MeshBasicMaterial( {color: 0x00ffff, transparent: true, opacity: 0.5} );
+    const plane = new THREE.Mesh( geometry, material );
+    anchor.group.add(plane);
+
+    mindarThree.start();
+    renderer.setAnimationLoop(() => {
+      renderer.render(scene, camera);
+    });
+
+    return () => {
+      renderer.setAnimationLoop(null);
+      mindarThree.stop();
+    }
+  }, []);
+
+  return (
+    <div style={{width: "100%", height: "100%"}} ref={containerRef}>
+    </div>
+  )
+}
+
