06af23cd69327165d6bb04fe743a7cd35a9581ef
Mohammad Asim Khan <ak4514768@gmail.com>
2023-06-03 15:28:11 +0000

    Added fesh-mesh filter component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import './App.css';
-import MindARViewer from './mindar-viewer';
-import MindARThreeViewer from './mindar-three-viewer';
+// import MindARViewer from './mindar-viewer';
+// import MindARThreeViewer from './mindar-three-viewer';
+import FaceMesh from './face-mesh';
 
 function App() {
   const [started, setStarted] = useState(null);
@@ -11,12 +12,13 @@ function App() {
       <h1>Example React component with <a href="https://github.com/hiukim/mind-ar-js" target="_blank">MindAR</a></h1>
 
       <div className="control-buttons">
-        {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
-        {started === null && <button onClick={() => {setStarted('three')}}>Start ThreeJS version</button>}
+        {/* {started === null && <button onClick={() => {setStarted('aframe')}}>Start AFRAME version</button>}
+        {started === null && <button onClick={() => { setStarted('three') }}>Start ThreeJS version</button>} */}
+        {started === null && <button onClick={() => { setStarted('face mesh') }}>Face Mesh</button>}
         {started !== null && <button onClick={() => {setStarted(null)}}>Stop</button>}
       </div>
 
-      {started === 'aframe' && (
+      {/* {started === 'aframe' && (
         <div className="container">
           <MindARViewer/>
           <video></video>
@@ -26,6 +28,12 @@ function App() {
       {started === 'three' && (
         <div className="container">
           <MindARThreeViewer />
+        </div>
+      )} */}
+      
+      {started === 'face mesh' && (
+        <div className="container">
+          <FaceMesh />
         </div>
       )}
     </div>
